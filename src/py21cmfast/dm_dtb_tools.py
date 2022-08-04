1cf3f8704e8014b7f4a9355af5177c05a5d6f83d
gaetanfacchinetti <gaetanfacc@gmail.com>
2022-08-04 19:20:43 +0200

    small modifications

--- src/py21cmfast/dm_dtb_tools.py
+++ src/py21cmfast/dm_dtb_tools.py
@@ -1,10 +1,21 @@
-###########################################################
-# New in exo21cmFAST
-# Gaetan Facchinetti: gaetan.facchinetti@ulb.be
+############################################################################
+#    Code to organise a database for the different parts
+#    Copyright (C) 2022  Gaetan Facchinetti
 #
-# Manage the inputs/outputs and the database of files (related to DM energy injection)
-# WARNING: Accessing/writting in the database is not optimised
-###########################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+############################################################################
+
 
 import os
 from pathlib import Path
